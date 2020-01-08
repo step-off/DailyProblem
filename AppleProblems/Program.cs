fd8e08a52b7c9ff93aafe363764c3dede70e9227
step-off <nofantasy12@gmail.com>
2020-01-08 11:53:43 +0500

    Add UnitTests project & AppleProblems project

---
+++ AppleProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppleProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
