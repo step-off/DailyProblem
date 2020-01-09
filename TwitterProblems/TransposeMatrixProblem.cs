489cd1656e244baf5f43ad629fd277bcf6a0b3ff
step-off <nofantasy12@gmail.com>
2020-01-09 09:26:32 +0500

    Solve transpose matrix problem

---
+++ TwitterProblems/TransposeMatrixProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterProblems
+{
+/*
+    Given a matrix, transpose it. 
+    Transposing a matrix means the rows are now the column and vice-versa.
+
+    Here's an example:
+
+    def transpose(mat):
+      # Fill this in.
+
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+
+    print(transpose(mat))
+    # [[1, 4],
+    #  [2, 5], 
+    #  [3, 6]]
+ */
+    public class TransposeMatrixProblem
+    {
+        public List<List<int>> Solve(List<List<int>> matrix)
+        {
+            var result = new List<List<int>>();
+            var columns = matrix.Count > 0 ? matrix[0].Count : 0;
+            for (int i = 0; i < columns; i++)
+            {
+                var transponedColumn = new List<int>();
+                foreach (var column in matrix)
+                {
+                    transponedColumn.Add(column[i]);
+                }
+                result.Add(transponedColumn);
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTests/TransposeMatrixProblem.Tests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProblems;
+
+namespace TwitterProblemsTests
+{
+    public class TransposeMatrixProblemTests
+    {
+        [Test]
+        public void TransposeMatrix_Test1()
+        {
+            var instance = new TransposeMatrixProblem();
+            var input = new List<List<int>>()
+            {
+                new List<int>() {1, 2, 3},
+                new List<int>() {4, 5, 6},
+                new List<int>() {7, 8, 9}
+            };
+            var expected = new List<List<int>>()
+            {
+                new List<int>() {1, 4, 7},
+                new List<int>() {2, 5, 8},
+                new List<int>() {3, 6, 9}
+            };
+
+
+            var actual = instance.Solve(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TransposeMatrix_Test2()
+        {
+            var instance = new TransposeMatrixProblem();
+            var input = new List<List<int>>()
+            {
+                new List<int>() {1, 2, 3, 4, 5, 6},
+                new List<int>() {7, 8, 9, 10, 11, 12}
+            };
+            var expected = new List<List<int>>()
+            {
+                new List<int>() {1, 7},
+                new List<int>() {2, 8},
+                new List<int>() {3, 9},
+                new List<int>() {4, 10},
+                new List<int>() {5, 11},
+                new List<int>() {6, 12},
+            };
+
+
+            var actual = instance.Solve(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TransposeMatrix_Test3()
+        {
+            var instance = new TransposeMatrixProblem();
+            var input = new List<List<int>>()
+            {
+                new List<int>() {1, 2, 3}
+            };
+            var expected = new List<List<int>>()
+            {
+                new List<int>() {1},
+                new List<int>() {2},
+                new List<int>() {3},
+            };
+
+
+            var actual = instance.Solve(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TransposeMatrix_Test4()
+        {
+            var instance = new TransposeMatrixProblem();
+            var input = new List<List<int>>();
+            var expected = new List<List<int>>();
+
+            var actual = instance.Solve(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
