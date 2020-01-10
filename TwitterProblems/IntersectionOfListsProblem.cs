bd8eb2a047d7aba5cb0e784a1fea08c941b17056
step-off <nofantasy12@gmail.com>
2020-01-10 08:26:11 +0500

    Solve `intersection of lists` problem

---
+++ TwitterProblems/IntersectionOfListsProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitterProblems
+{
+/*
+    Given sorted lists, find the intersection of those lists.
+
+    Here's an example and some starter code.
+
+    def intersection(list1, list2, list3):
+      # Fill this in.
+
+    print(intersection([1, 2, 3, 4], [2, 4, 6, 8], [3, 4, 5]))
+    # [4]
+*/
+    public class IntersectionOfListsProblem
+    {
+        public T[] Solve<T>(params T[][] lists)
+        {
+            var result = new HashSet<T>();
+            var dict = new Dictionary<T, HashSet<int>>();
+            var listsLength = lists.Length;
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                var list = lists[i];
+                for (int j = 0; j < list.Length; j++)
+                {
+                    var listItem = list[j];
+                    if (dict.ContainsKey(listItem))
+                        dict[listItem].Add(i);
+                    else
+                        dict[listItem] = new HashSet<int>() { i };
+                    if (dict[listItem].Count == listsLength)
+                        result.Add(listItem);
+                }
+            }  
+            return result.ToArray();
+        }
+    }
+}
---
+++ UnitTests/IntersectionOfLists.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TwitterProblems;
+
+namespace TwitterProblemsTests
+{
+    public class IntersectionOfListsProblemTests
+    {
+        [Test]
+        public void IntersectionOfLists_Test1()
+        {
+            var instance = new IntersectionOfListsProblem();
+
+            var expected = new int[] { 4 };
+            var actual = instance.Solve(
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 2, 4, 6, 8 },
+                new int[] { 3, 4, 5 }
+              );
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IntersectionOfLists_Test2()
+        {
+            var instance = new IntersectionOfListsProblem();
+
+            var expected = new int[] { 4 };
+            var actual = instance.Solve(
+                new int[] { 1, 1, 2, 2, 4, 4 },
+                new int[] { 2, 4, 4, 6, 6, 8, 8 },
+                new int[] { 3, 3, 4, 4, 5, 5 }
+              );
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IntersectionOfLists_Test3()
+        {
+            var instance = new IntersectionOfListsProblem();
+
+            var expected = new string[] { "Hello" };
+            var actual = instance.Solve(
+                new string[] { "Hello", "i", "am", "here" },
+                new string[] { "Hello", "there" },
+                new string[] { "Hello", "nope" }
+              );
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IntersectionOfLists_Test4()
+        {
+            var instance = new IntersectionOfListsProblem();
+
+            var expected = new string[] { "Hello", "i" };
+            var actual = instance.Solve(
+                new string[] { "Hello", "i", "am", "here" },
+                new string[] { "Hello", "there", "i" },
+                new string[] { "Hello", "nope", "i" }
+              );
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
